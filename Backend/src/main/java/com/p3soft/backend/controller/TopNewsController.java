b7c0b58a7e95dbde6164e0273fa1c849c8096e97
ali-khosravani <ali.p3soft@gmail.com>
2023-04-04 03:44:57 +0330

    update program

--- Backend/src/main/java/com/p3soft/backend/controller/TopNewsController.java
+++ Backend/src/main/java/com/p3soft/backend/controller/TopNewsController.java
@@ -2,22 +2,33 @@ package com.p3soft.backend.controller;
 
 import com.p3soft.backend.model.TopNews;
 import com.p3soft.backend.repository.TopNewsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/TopNews")
 public class TopNewsController {
-    @Autowired
+
     private TopNewsRepository topNewsRepository;
 
+    TopNewsController(TopNewsRepository topNewsRepository){
+        this.topNewsRepository= topNewsRepository;
+    }
+
     @CrossOrigin(origins = {"*"},maxAge = 4800 , allowCredentials = "false")
+
+
 
     @GetMapping("/TopNews")
     public List<TopNews> getAllTopNews(){
         return topNewsRepository.findAll();
+    }
+
+    @CrossOrigin(origins = {"*"},maxAge = 4800 , allowCredentials = "false")
+    @GetMapping("/TopNews/{id}")
+    public  TopNews one(@PathVariable Long id){
+        return  topNewsRepository.findById(id)
+                .orElseThrow();
     }
 }
